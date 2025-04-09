58b9e30f1f4fdfee03fcddc8a7c4438db119d837
fkcicinho <139833369+fkcicinho@users.noreply.github.com>
2025-04-09 01:17:53 -0300

    Add files manualmente
    Arquivos fonte

---
+++ Menu_drivers.ps1
@@ -0,0 +1,104 @@
+﻿cls
+
+
+$default_path = "c:\program files\server_drivers" # Trocar pelo hostname da máquina que será o server
+
+# Construção das pastas 
+$val_path  = Test-Path -path $default_path 
+
+
+
+if ($val_path -eq $false){
+        mkdir $default_path
+
+}
+
+
+# Obtendo os dados da máquina
+$model = Get-WmiObject win32_computersystem
+$model = $model.Model
+
+
+Write-Host "
+(1) Install drivers local
+(2) Extrair drivers local
+(3) Install drivers em rede
+(4) extrair drivers em rede
+(5) Sair
+"
+
+$options = Read-Host "Escolha uma opção "
+
+
+
+switch ($options) {
+# install drivers local
+
+"1" {
+        $val_model = Test-Path -Path "$default_path\$model"
+          
+       
+        # Não existe os drivers no servidor
+        
+        if ($val_model -eq $false) {msg * /w "Driver do modelo: $Model não foram encontrados no servidor" ; exit} 
+        
+        else {
+               
+               gci "$default_path\$model" -Recurse -Filter *.inf
+
+               pause
+
+                
+
+        
+        } 
+
+
+        
+
+    
+}
+
+# Extração dos drivers local
+"2" {
+        $path_model = "$default_path\$model"
+
+
+        md $path_model # Criação da pasta no servidor
+
+
+        $drivers = pnputil /enum-drivers
+
+
+        $filtro_drivers = $drivers | Select-string "Class Name"
+
+
+        $filtro_drivers | ForEach-Object {
+            
+            $teste = Test-Path -path "$path_model\$_" 
+
+            write-host "Dessa forma que esta vindo o resultado: $path_model\$_" 
+
+            
+
+            pause
+            
+            if ($teste -eq $false) {md "$path_model\$_" }
+        
+        
+        }
+
+
+}
+
+
+
+default {
+    write-host "Opção incorreta"
+
+}
+
+
+
+
+}
---
+++ folder_organization.ps1
@@ -0,0 +1,33 @@
+﻿cls
+
+
+
+
+# Caminho onde os drivers foram exportados
+$sourcePath = "c:\temp\drivers"
+# Caminho onde você quer organizar os drivers
+$destinationPath = "c:\temp\teste"
+
+# Crie a pasta de destino, caso não exista
+if (-not (Test-Path -Path $destinationPath)) {
+    New-Item -Path $destinationPath -ItemType Directory
+}
+
+# Obter todos os drivers exportados
+$drivers = Get-ChildItem -Path $sourcePath
+
+# Para cada driver, mova para a pasta da classe
+foreach ($driver in $drivers) {
+    # Obtenha a classe do dispositivo (isso pode ser ajustado para ler do arquivo de driver ou do WMI)
+    $classGuid = (Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.InfName -eq $driver.Name }).ClassGuid
+    $classFolder = [System.Guid]::NewGuid($classGuid).ToString()
+
+    # Crie uma pasta para a classe, se não existir
+    $classFolderPath = Join-Path -Path $destinationPath -ChildPath $classFolder
+    if (-not (Test-Path -Path $classFolderPath)) {
+        New-Item -Path $classFolderPath -ItemType Directory
+    }
+
+    # Mover o driver para a pasta correspondente à classe
+    Move-Item -Path $driver.FullName -Destination $classFolderPath
+}
